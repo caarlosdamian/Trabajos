df846797ac4e038a6e6c669c6d8cf58d05242b1b
Carlos Damian <dapecarlos@gmail.com>
2017-09-08 20:29:36 -0500

    Chapter 12 update 12.4

--- Chapter_12/12.4.rb
+++ Chapter_12/12.4.rb
@@ -4,14 +4,19 @@ filename = 'names.txt'
 read_array = File.read filename
 
 puts read_array
-
+time_actual = Time.new
+time_actual_año = Time.new.year
 name = gets.chomp.downcase
-
-
+age = 1
 read_array.each_line do |d|
-    name_searching = d.split ','
-    if name_searching[0].downcase == name.downcase
-    puts name_searching[1]
+ name_searching = d.split ','
+ if name_searching[0].downcase == name.downcase
+    temp =  name_searching[2].to_s
+    while Time.mktime(temp.to_i + age)<= time_actual
+      age = age + 1
+    end
+    puts "Tu proxima fecha de cumpleaños sera : #{name_searching[1]} #{time_actual_año}"
+    puts "Tiene #{age} años"
   end
-puts "-----"
+
 end
