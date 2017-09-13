a74b83fed9abec49fa4a8e66cfc0260d22dc0168
Carlos Damian <dapecarlos@gmail.com>
2017-09-13 10:04:17 -0500

    chapter 13

--- Chapter_13/13.1.rb
+++ Chapter_13/13.1.rb
@@ -1,35 +1,62 @@
 
-
-
-  class Integer
-    list = []
-    word = 'one'
-    while word != ''
-      word = gets.chomp
-      list.push word 
-    def do_shuffel 
-      randm = []
-      count = -2
-      x = 0
-      y = 0
-      self.each do |word|
-        count  = count + 1
+class Orange_tree
+  
+    def initialize
+      @height =2
+      @age = 1
+      @orange
+    end
+    def height
+       puts @height 
+    end
+    def one_year_passes
+      @age += 1
+      @height += 3
+      @orange = 0
+      if @age > 3
+        @orange = 2
       end 
-      while y <= count 
-        x = rand(count+1)
-        if self[x] != 'used'
-          randm.push self[x]
-          self [] = 'used'
-          y = y + 1
+      if @age >=7
+        puts "tree have to die"
+        exit
+      end 
+      puts "one year pass"
+      puts "your tree have #{@age}"
+    end 
+    def count_of_oranges
+      if @age >= 3
+         @orage = 2
+        elsif @age >=5
+        @orage = 3
         end
+    end 
+  
+    def pick_up_orange
+      if @orange > 0
+        @orange -= 1
+        puts "you pick up #{@orange}" 
+      else
+        puts
+        "there is no more oranges"
       end 
-      puts randm
     end 
   end 
-end
-  list.do_shuffel
   
-
-
-
-
+  tree = Orange_tree.new
+  tree.height
+  tree.one_year_passes
+  tree.height
+  tree.count_of_oranges
+  tree.one_year_passes
+  tree.pick_up_orange
+  tree.one_year_passes
+  tree.height
+  tree.pick_up_orange
+  tree.count_of_oranges
+  tree.one_year_passes
+  tree.pick_up_orange
+  tree.pick_up_orange
+  tree.one_year_passes
+  tree.one_year_passes  
+  tree.one_year_passes
+  
